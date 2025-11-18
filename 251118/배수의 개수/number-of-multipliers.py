4e2e0e4bd9e4dcba0c84133c5a5ab4c9270efbdc
kimnamhyeong01 <119601427+kimnamhyeong01@users.noreply.github.com>
2025-11-18 18:43:06 +0900

    Title: 배수의 개수, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251118/배수의 개수/number-of-multipliers.py
@@ -0,0 +1,13 @@
+cnt_3 = 0
+cnt_5 = 0
+for _ in range(10):
+    a = int(input())
+    if a % 3 == 0:
+        cnt_3 += 1
+        continue
+    elif a % 5 == 0:
+        cnt_5 += 1
+        continue
+    else:
+        continue 
+print(cnt_3, cnt_5)
