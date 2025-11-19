8027190276013f141064eb754b3e9cb8a61f0b3f
kimnamhyeong01 <119601427+kimnamhyeong01@users.noreply.github.com>
2025-11-19 15:08:35 +0900

    Title: 요일 맞추기, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 251119/요일 맞추기/guess-day-of-week.py
+++ 251119/요일 맞추기/guess-day-of-week.py
@@ -1,35 +1,20 @@
 m1, d1, m2, d2 = map(int, input().split())
-num_days = [0, 'Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 0]
-day = 2
-num_months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-month = m1 
-date = d1
 
-while True:
-    if month == m2:
-        if date == d2:
-            print(num_days[day])
-            break
-        elif date > d2:
-            date -= 1
-            day -= 1
-            if day == 0:
-                day = 7
-    elif month > m2:
-        if date == 0:
-            month -= 1
-            date = num_months[month]
-            day -= 1
-        if day == 0:
-            day = 7
-        date -= 1
-        day -= 1
-    elif month < m2:
-        if date == num_months[month]:
-            month += 1
-            date += 1
-            day += 1
-        if day == 8:
-            day = 1
-        date += 1
-        day += 1
+def num_of_days(m, d):
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31] 
+    total_days = 0
+
+    for i in range(1, m):
+        total_days += days[i]
+    
+    total_days += d 
+
+    return total_days 
+
+diff = num_of_days(m2, d2) - num_of_days(m1, d1)
+
+while diff < 0:
+    diff += 7
+
+day_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+print(day_of_week[diff % 7])
