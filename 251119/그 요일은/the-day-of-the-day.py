9c46e89062964a3d73d957ec7f70d9c19a2cab19
kimnamhyeong01 <119601427+kimnamhyeong01@users.noreply.github.com>
2025-11-19 15:55:29 +0900

    Title: 그 요일은, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 251119/그 요일은/the-day-of-the-day.py
+++ 251119/그 요일은/the-day-of-the-day.py
@@ -1,33 +1,26 @@
 m1, d1, m2, d2 = map(int, input().split())
 A = input()
+
 month = m1 
 date = d1
+
 cnt = 0
 day = 0 
 week_day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] 
 month_date = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-while month != m2:
-    if month == m2 and date == d2:
-        break
-    if A == week_day[day]:
-        cnt += 1
-    if date == month_date[month]:
-        date = 0
-        month += 1 
-    if day == 6:
-        day = 0
-    date += 1
-    day += 1
- 
-while month == m2 and date != d2:
-    if date == d2:
-        break  
-    if A == week_day[day]:
-        cnt += 1 
-    if day == 6:
-        day = 0
-    date += 1
-    day += 1 
 
-if month == m2 and date == d2:
-    print(cnt)
+while True:
+    if week_day[day] == A:
+        cnt += 1
+    
+    if month == m2 and date == d2:
+        break 
+    
+    date += 1 
+    if date > month_date[month]:
+        date = 1
+        month += 1
+    
+    day = (day + 1) % 7
+
+print(cnt)
