c739d1966250d4f6783ad9d1ba868acbf124800f
kimnamhyeong01 <119601427+kimnamhyeong01@users.noreply.github.com>
2025-11-19 18:36:19 +0900

    Title: 오름 내림차순 정렬, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 251119/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,11 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+nums.sort()
+for i in nums:
+    print(i, end=' ')
+print('')
+nums.reverse()
+for i in nums:
+    print(i, end=' ')
+# Please write your code here.
