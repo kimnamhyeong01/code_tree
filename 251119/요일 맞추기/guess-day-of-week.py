abb174a7de5935427bb679127cb010a50bc27c56
kimnamhyeong01 <119601427+kimnamhyeong01@users.noreply.github.com>
2025-11-19 14:36:47 +0900

    Title: 요일 맞추기, Time: 1099ms, Memory: 16MB, Status: Time Limit Exceed - Codetree

--- 251119/요일 맞추기/guess-day-of-week.py
+++ 251119/요일 맞추기/guess-day-of-week.py
@@ -6,27 +6,15 @@ month = m1
 date = d1
 
 while True:
-    if month == m2 and date == d2:
-        print(num_days[day])
-        break 
-    elif month == m2 and date > d2:
-        if date == 0:
-            month -= 1
-            date = num_months[month]
+    if month == m2:
+        if date == d2:
+            print(num_days[day])
+            break
+        elif date > d2:
+            date -= 1
             day -= 1
-        if day == 0:
-            day = 7 
-        date -= 1
-        day -= 1
-    elif month == m2 and date < d2:
-        if date == num_months[month]:
-            month += 1
-            date = 1
-            day += 1
-        if day == 8:
-            day = 1
-        date += 1 
-        day += 1 
+            if day == 0:
+                day = 7
     elif month > m2:
         if date == 0:
             month -= 1
