c046e6167704c4218e6bd394a77ea0f86c4d2c6c
kimnamhyeong01 <119601427+kimnamhyeong01@users.noreply.github.com>
2025-11-19 15:47:17 +0900

    Title: 그 요일은, Time: 46ms, Memory: 23MB, Status: Wrong Answer - Codetree

--- 251119/그 요일은/the-day-of-the-day.py
+++ 251119/그 요일은/the-day-of-the-day.py
@@ -7,27 +7,27 @@ day = 0
 week_day = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] 
 month_date = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 while month != m2:
-    if month == m2:
+    if month == m2 and date == d2:
         break
     if A == week_day[day]:
         cnt += 1
     if date == month_date[month]:
         date = 0
-        month += 1
-    date += 1
-    day += 1
+        month += 1 
     if day == 6:
         day = 0
+    date += 1
+    day += 1
  
-while date != d2:
+while month == m2 and date != d2:
     if date == d2:
         break  
     if A == week_day[day]:
         cnt += 1 
-    date += 1
-    day += 1 
     if day == 6:
         day = 0
+    date += 1
+    day += 1 
 
 if month == m2 and date == d2:
     print(cnt)
