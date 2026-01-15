708fcdc2d30c1beaa8130418ff1c2c0000319835
NamHyeong Kim <119601427+kimnamhyeong01@users.noreply.github.com>
2026-01-15 19:39:21 +0900

    Title: 두 배열의 곱, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260115/두 배열의 곱/multiple-of-two-arrays.py
+++ 260115/두 배열의 곱/multiple-of-two-arrays.py
@@ -5,11 +5,13 @@ for _ in range(3):
     rows_1 = list(map(int, input().split()))
     array1.append(rows_1)
 
-for _ in range(3):
+for _ in range(4):
     rows_2 = list(map(int, input().split()))
     array2.append(rows_2) 
+array2.pop(0) 
 
 for i in range(3):
     for j in range(3):
         print(array1[i][j] * array2[i][j], end=' ')
     print()
+    
